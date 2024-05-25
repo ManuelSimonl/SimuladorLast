c06bb722f0dea3e66a5bae34558d7985afd66387
ManuelSimonl <157650275+ManuelSimonl@users.noreply.github.com>
2024-05-25 20:52:18 +0200

    Delete found directory

--- found/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.simulador {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.simuladorv to javafx.fxml;
-    exports com.example.simuladorv;
-}
